1d0a1f5604d6110471f1ce5a92ce46d76e93377a
Dong Hyuk Park <sts04038@gmail.com>
2023-07-26 10:16:23 +0900

    settings.py DATABASE 변경

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'mysite2.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mysite2",
+        "USER": "mysite2",
+        "PASSWORD": "1234",
     }
 }
+
 
 
 # Password validation
